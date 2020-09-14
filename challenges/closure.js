1b917c15a77f656a2d7741cfe938790b4d63d142
xandre9-git <jmjames673@gmail.com>
2020-09-13 20:43:40 -0400

    completed closures section

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,17 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: Functions are able to grab variables outside of its own scope.
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(param){
+  let sum = 0
+  for (let i = 0; i <= param; i++){
+    sum += i
+  }
+  return sum;
+}
